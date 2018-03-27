2a16197d25f4e7d80dd6b2c49e2ebbbe5dabbe98
Assisneto <netodamasceno12@gmail.com>
2018-03-27 11:39:47 -0300

    Configurando primeira rota

--- app.js
+++ app.js
@@ -1,4 +1,8 @@
 let express = require('express');
 let app = express();
 
+app.get('/produtos', (req,res)=>{
+    res.send("<html><body><h1>Listagem de produtos</h1></body></html>");
+});
+
 app.listen(3000,()=> console.log("servidor rodando"));
