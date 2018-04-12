daca8db079dac29a60619bde66b7b6c7792480d1
Assisneto <netodamasceno12@gmail.com>
2018-04-12 19:21:03 -0300

    primeiro TDD

--- test/produtos.js
+++ test/produtos.js
@@ -1,6 +1,19 @@
-describe("#Produtos-controller",()=>{
-    it('#Listagem json',()=>{
-        console.log("teste de verificação de listagem do json");
-        
+let http = require('http');
+let assert = require('assert');
+describe('#ProdutosController', ()=> {
+    it('#listagem de produtos json', (done) => {
+        var configuracoes = {
+            hostname: 'localhost',
+            port: 3000,
+            path: '/produtos',
+            headers: {
+                'Accept':'application/json'
+            }
+        };
+        http.get(configuracoes, function(res){
+            assert.equal(res.statusCode, 302);
+            assert.equal(res.headers['content-type'], 'application/json; charset=utf-8');
+            done();
+        });
     });
 });
