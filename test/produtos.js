2f707bb29ec568ffa31d1892a90651e4dac99bc7
Assisneto <netodamasceno12@gmail.com>
2018-04-20 23:19:47 -0300

    test

--- test/produtos.js
+++ test/produtos.js
@@ -2,26 +2,26 @@ let express = require('../config/express')();
 let request = require("supertest")(express);
 
 describe('#ProdutosController', ()=> {
+    it('Listagem de produtos html', (done)=>{
+        request.get('/produtos')
+        .expect('Content-Type',/html/)
+        .expect(200,done);
+    });
     it('#listagem de produtos json', (done)=> {
         request.get('/produtos')
             .set('Accept', 'application/json')
             .expect('Content-Type', /json/)
             .expect(200,done);
         });
-    it('Listagem de produtos html', (done)=>{
-        request.get('/produtos')
-            .expect('Content-Type',/html/)
-            .expect(200,done);
-        });
-    it('#Cadastro de produto invalido', (done)=>{
-        request.post('/produtos')
-            .send({titulo:"", descricao:"dasds"})
-            .expect(400,done);
-        });
     it('#Cadastro de produto valido',(done)=>{
         request.post('/produtos')
         .send({titulo:"nodepy", descricao:"node com python",preco:20.10})
         .expect(302,done);
     });      
+    it('#Cadastro de produto invalido', (done)=>{
+        request.post('/produtos')
+            .send({titulo:"", descricao:"dasds"})
+            .expect(400,done);
+        });
 });
 
