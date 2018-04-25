1fc87eae3ab718479c1e4654890bf82006f0b864
assisneto <netodamasceno12@gmail.com>
2018-04-24 22:55:39 -0300

    Criando middlewares

--- config/express.js
+++ config/express.js
@@ -23,5 +23,16 @@ module.exports = ()=> {
             .then('routes')
             .into(app);
 
+    app.use((req, res, next)=>{
+        res.status(404).render("erros/404");
+    });
+
+    app.use((error, req, res, next)=>{
+    if(process.env.NODE_ENV == 'production') {
+            res.status(500).render('erros/500');
+            return;
+    }
+        next(error);
+    });
     return app;
 }
